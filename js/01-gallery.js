ac1a83036855463c6dfaecd202de08eb7b1fc50b
Nataliia_Rud <nrud1981@gmail.com>
2023-02-14 14:55:12 +0200

    initial commit

---
+++ js/01-gallery.js
@@ -0,0 +1,15 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+console.log(galleryItems);
+console.log(galleryItems);
+console.log(galleryItems);
+console.log(galleryItems);
+console.log(galleryItems);
+console.log(galleryItems);
+console.log(galleryItems);
+console.log(galleryItems);
+console.log(galleryItems);
+console.log(galleryItems);
+console.log(galleryItems);
