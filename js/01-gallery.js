d3bc883c956edd3573088b456efdddd7a38711cf
Nataliia_Rud <nrud1981@gmail.com>
2023-02-15 15:33:44 +0200

    1st gallery ready

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,15 +1,45 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryElem = document.querySelector(".gallery");
 
-console.log(galleryItems);
-console.log(galleryItems);
-console.log(galleryItems);
-console.log(galleryItems);
-console.log(galleryItems);
-console.log(galleryItems);
-console.log(galleryItems);
-console.log(galleryItems);
-console.log(galleryItems);
-console.log(galleryItems);
-console.log(galleryItems);
-console.log(galleryItems);
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item"><a class="gallery__link href = "${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"></a></div>`
+  )
+  .join("");
+
+galleryElem.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryElem.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const urlPopup = event.target.dataset.source;
+
+  const instance = basicLightbox.create(
+    `
+		<img width="1280" height=auto src="${urlPopup}">
+	`,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", onKeyCloser);
+      },
+
+      onClose: (instance) => {
+        window.removeEventListener("keydown", onKeyCloser);
+      },
+    }
+  );
+
+  instance.show();
+
+  function onKeyCloser(event) {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryElem = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+  )
+  .join("");
+galleryElem.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryElem.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(event) {
+    if (event.target.node)
+}
